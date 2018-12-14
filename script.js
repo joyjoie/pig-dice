148d4b3b0f3c9e17bec7dd447d5e042b87d20ea1
joy <wendokingoo@gmail.com>
2018-12-14 19:23:19 +0300

    style elements added in css

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
       // $("div.win").show();
     //
         $("button#btn1").click(function(event) {
-
+          $("button#start").hide();
           var dies = $("#die1","#die2");
           var status = $("#status1","status2");
           var d1 = Math.floor(Math.random() * 6) + 1;
@@ -24,22 +24,22 @@ $(document).ready(function() {
           $('#die1').html() = d1;
           $('#die2').html() = d2;
           $('#status').html() = "You rolled" + diceTotal + " .";
-    //       if (playerscore1 == playerscore2) {
-    //         $('#status'.html() += "you scored the same !! you get a free turn ;)";
-    //
+          if (d1 == d2) {
+            $('#status'.html() += "yougot the same values ;)";
+
     //       }
-    //       if(player1)
-
-          $("button#btn2").click(function(event){
-
-            var status = $("#status1","status2");
-            var d1 = Math.floor(Math.random() * 6) + 1;
-            var d2 = Math.floor(Math.random() * 6) + 1;
-            var playerdiceTotal = $('d1 +d2');
-            var initialScore =0;
-            $('#die1').html() = d1;
-            $('#die2').html() = d2;
-            $('#status').html() = "You rolled" + diceTotal + " .";
+    // //       if(player1)
+    //
+    //       $("button#btn2").click(function(event){
+    //       $("button#start").hide();
+    //         var status = $("#status1","status2");
+    //         var d1 = Math.floor(Math.random() * 6) + 1;
+    //         var d2 = Math.floor(Math.random() * 6) + 1;
+    //         var playerdiceTotal = $('d1 +d2');
+    //         var initialScore =0;
+    //         $('#die1').html() = d1;
+    //         $('#die2').html() = d2;
+    //         $('#status').html() = "You rolled" + diceTotal + " .";
           }):
           });
         });
