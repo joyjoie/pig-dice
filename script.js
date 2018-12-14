43639a956d181288a3a339f1fd72a07134c5ac54
joy <wendokingoo@gmail.com>
2018-12-14 18:50:27 +0300

    bootstrap file added and columns modified

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 // // business logic
-$(document).ready(function) {
-    $("button#start").onclick(function(event)) {
-        event.preventDefault();
-        ("button#reset").hide();
-        ("button#btn").hide();
-
-        $("button#btn").onclick(function(event)) {
-          event.preventDefault();
-          var die1 = $("#die1");
-          var die2 = $("#die2");
-          var status = $("#status");
-          var turn= $("#turns");
-          var wins =$("#winner");
-          var score =$("#score1","score2")
-          var d1 = Math.floor(Math.random() * 6) + 1;
-          var d2 = Math.floor(Math.random() * 6) + 1;
-          var diceTotal = $('d1 +d2');
-          var initialScore =0;
-          $(die1).html() = d1;
-          $(die2).html() = d2;
-          status.html() = "You rolled" + diceTotal + " .";
-          if (d1 == d2) {
-            status.html() += "The same !! you get a free turn ;)";
-          }
-          if(d)
-
-        }
+$(document).ready(function() {
+  $("button#reset").hide();
+  $("button#btn").hide();
+  $("div.play1").hide();
+  $("div.play2").hide();
+  $("div.win").hide();
+  $("div.dices").hide();
+    $("button#start").click(function(event) {
+      $("button#reset").show();
+      $("button#btn").show();
+      $("div.play1").show();
+      $("div.play2").show();
+      // $("div.win").show();
+    //
+        $("button#btn").click(function(event) {
+    //       event.preventDefault();
+    //       var dies = $("#die1","#die2");
+    //       var players=$("#player1","player2");
+    //       var status = $("#status1","status2");
+    //       var turn= $("#turns");
+    //       var wins =$("#winner");
+    //       var score =$("#score1","score2")
+    //       var d1 = Math.floor(Math.random() * 6) + 1;
+    //       var d2 = Math.floor(Math.random() * 6) + 1;
+    //       var playerdiceTotal = $('d1 +d2');
+    //       var initialScore =0;
+    //       $('#die1').html() = d1;
+    //       $('#die2').html() = d2;
+    //       $('#play1').html()=playe1;
+    //       $('#play2').html()=playe2;
+    //       $('#score1').html()=playerscore1;
+    //       $('#score2').html()=playerscore2;
+    //       $('#status').html() = "You rolled" + diceTotal + " .";
+    //       if (playerscore1 == playerscore2) {
+    //         $('#status'.html() += "you scored the same !! you get a free turn ;)";
+    //
+    //       }
+    //       if(player1)
+          });
+        });
+      });
