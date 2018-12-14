a353b1b1382669421e735a6c06f5c8e55de021b2
joy <wendokingoo@gmail.com>
2018-12-14 19:02:17 +0300

    button and respective id's added to every column

--- script.js
+++ script.js
@@ -13,30 +13,42 @@ $(document).ready(function() {
       $("div.play2").show();
       // $("div.win").show();
     //
-        $("button#btn").click(function(event) {
+        $("p#Roll1").click(function(event) {
     //       event.preventDefault();
     //       var dies = $("#die1","#die2");
     //       var players=$("#player1","player2");
-    //       var status = $("#status1","status2");
+          var status = $("#status1","status2");
     //       var turn= $("#turns");
     //       var wins =$("#winner");
     //       var score =$("#score1","score2")
-    //       var d1 = Math.floor(Math.random() * 6) + 1;
-    //       var d2 = Math.floor(Math.random() * 6) + 1;
-    //       var playerdiceTotal = $('d1 +d2');
-    //       var initialScore =0;
-    //       $('#die1').html() = d1;
-    //       $('#die2').html() = d2;
+          var d1 = Math.floor(Math.random() * 6) + 1;
+          var d2 = Math.floor(Math.random() * 6) + 1;
+          var playerdiceTotal = $('d1 +d2');
+          var initialScore =0;
+          $('#die1').html() = d1;
+          $('#die2').html() = d2;
     //       $('#play1').html()=playe1;
     //       $('#play2').html()=playe2;
     //       $('#score1').html()=playerscore1;
     //       $('#score2').html()=playerscore2;
-    //       $('#status').html() = "You rolled" + diceTotal + " .";
+          $('#status').html() = "You rolled" + diceTotal + " .";
     //       if (playerscore1 == playerscore2) {
     //         $('#status'.html() += "you scored the same !! you get a free turn ;)";
     //
     //       }
     //       if(player1)
+
+          $("p#Roll2").click(function(event){
+                $("p#Roll1").hide();
+            var status = $("#status1","status2");
+            var d1 = Math.floor(Math.random() * 6) + 1;
+            var d2 = Math.floor(Math.random() * 6) + 1;
+            var playerdiceTotal = $('d1 +d2');
+            var initialScore =0;
+            $('#die1').html() = d1;
+            $('#die2').html() = d2;
+            $('#status').html() = "You rolled" + diceTotal + " .";
+          }):
           });
         });
       });
