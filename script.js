532b885488839338be66058bc9a7b0e57ab149cd
joy <wendokingoo@gmail.com>
2018-12-14 15:59:26 +0300

    jquery element added in script tag

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-//business logic
-function rolldie(){
-var die1= document.getElementById("die1");
-var die2 = document.getElementById("die2");
-  var d1 = Math.floor(Math.random()*6)+1;
-  var d2 =Math.floor(Math.random()*6)+1;
-  var diceTotal =d1 +d2;
-  die1.innerHTML = d1;
-  die2.innerHTML = d2;
+// // business logic
+$(document).ready(function) {
+    $("button#start").onclick(function(event)) {
+        event.preventDefault();
+        ("button#reset").hide();
+        ("button#btn").hide();
 
-}
+        $("button#btn").onclick(function(event)) {
+          event.preventDefault();
+          var die1 = $("#die1");
+          var die2 = $("#die2");
+          var status = $("#status");
+          var turn= $("#turns");
+          var wins =$("#winner");
+          var score =$("#score1","score2")
+          var d1 = Math.floor(Math.random() * 6) + 1;
+          var d2 = Math.floor(Math.random() * 6) + 1;
+          var diceTotal = $('d1 +d2');
+          var initialScore =0;
+          $(die1).html() = d1;
+          $(die2).html() = d2;
+          status.html() = "You rolled" + diceTotal + " .";
+          if (d1 == d2) {
+            status.html() += "The same !! you get a free turn ;)";
+          }
+          if(d)
+
+        }
