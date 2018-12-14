d499235e9a60527f9cc78832ac914bad5aafae7a
joy <wendokingoo@gmail.com>
2018-12-14 09:42:44 +0300

    Elements added to script and html documents

---
+++ script.js
@@ -0,0 +1,8 @@
+//business logic
+function rolldie(){
+var player1,player2
+player1= parseInt(Math.random()*6+1)
+player2 = parseInt(Math.random()*6+1)
+  var x =document.getElementsById(die);
+  x=player1 + " and" + player2;
+}
