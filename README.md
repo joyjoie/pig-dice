ff82af0b0d2525cc15b5b4a8e3af61c8a413909c
joy <wendokingoo@gmail.com>
2018-12-17 10:22:33 +0300

    readme created and changes made for the readme file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pigdice
+#### By Joy King'oo
+## Description
+this is a game that allows a user to roll two die. if one player rolls the dice and gets similar numbers they get an extra turn.if one of the player rolls the die and its output is one,the player has to begin playing the game again. Whoever reaches the highest point is the winner.
+## Behaviour Driven Development
+* Gather choice from input given by users
+* Validate total number of wins from the
+* If Validation & Verification of questions fail, Show Error Message
+* Else display results of sum of questions answered correctly
+## Known Bugs
+No known bugs
+## Technologies Used
+HTML Javascript and CSS have been used for development
+## Support and contact details
+if you have any concerns or issues regarding my current work reach out through my github account
+### MIT License
+Copyright (c) 2018 **Joywendo**
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // // business logic
 $(document).ready(function() {
-  $("button#reset").hide();
-  $("button#btn").hide();
-  $("div.play1").hide();
-  $("div.play2").hide();
-  $("div.win").hide();
-  $("div.dices").hide();
-    $("button#start").click(function(event) {
-      $("button#reset").show();
-      $("button#btn").show();
-      $("div.play1").show();
-      $("div.play2").show();
-      // $("div.win").show();
-    //
+      $("button#reset").hide();
+      $("button#btn").hide();
+      $("div.play1").hide();
+      $("div.play2").hide();
+      $("div.win").hide();
+      $("div.dices").hide();
+      $("button#start").click(function(event) {
+        // $("button#reset").show();
+        // $("button#btn").show();
+        // $("div.play1").show();
+        // $("div.play2").show();
+        // $("div.win").show();
+        //
         $("button#btn1").click(function(event) {
           $("button#start").hide();
           var die1 = $("#die1");
@@ -26,22 +26,28 @@ $(document).ready(function() {
           $('#die2').html() = d2;
           $('#status').html() = "You rolled" + diceTotal + " .";
           if (d1 == d2) {
-            $('#status'.html() += "you get a second try you earned interval ;)";
+            $('#status'.html() += "you get a second try you earned it ;)";
 
-          }
-    // //       if(player1)
-    //
-    //       $("button#btn2").click(function(event){
-    //       $("button#start").hide();
-    //         var status = $("#status1","status2");
-    //         var d1 = Math.floor(Math.random() * 6) + 1;
-    //         var d2 = Math.floor(Math.random() * 6) + 1;
-    //         var playerdiceTotal = $('d1 +d2');
-    //         var initialScore =0;
-    //         $('#die1').html() = d1;
-    //         $('#die2').html() = d2;
-    //         $('#status').html() = "You rolled" + diceTotal + " .";
-          }):
-          });
+            }
+
+            $("button#btn2").click(function(event) {
+                $("button#start").hide();
+                var die1 = $("#die1");
+                var die2 = $("#die2");
+                var status = $("#status");
+                var d1 = Math.floor(Math.random() * 6) + 1;
+                var d2 = Math.floor(Math.random() * 6) + 1;
+                var diceTotal = $('d1 +d2');
+                // var initialScore =0;
+                $('#die1').html() = d1;
+                $('#die2').html() = d2;
+                $('#status').html() = "You rolled" + diceTotal + " .";
+                if (d1 == d2) {
+                  $('#status'.html() += "you get a second try you earned it ;)";
+
+
+                  }
+                });
+            });
         });
       });
